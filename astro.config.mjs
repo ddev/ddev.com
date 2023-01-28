563e7c6663d060db050ba01174afe3d3e4d2515e
Matt Stein <m@ttste.in>
2023-01-27 16:39:52 -0800

    Add `rel=“prefetch”` support.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,31 @@
-import { defineConfig } from "astro/config"
-import { existsSync } from "fs"
-import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs"
-import { plainTextPlugin } from "@barnabask/astro-minisearch"
-import tailwind from "@astrojs/tailwind"
-import react from "@astrojs/react"
-import robotsTxt from "astro-robots-txt"
-import sitemap from "@astrojs/sitemap"
-import searchIndex from "./src/lib/search-index.js"
-import widont from "rehype-widont"
+import { defineConfig } from "astro/config";
+import { existsSync } from "fs";
+import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
+import { plainTextPlugin } from "@barnabask/astro-minisearch";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
+import searchIndex from "./src/lib/search-index.js";
+import widont from "rehype-widont";
+import prefetch from "@astrojs/prefetch";
 
+/**
+ * A tiny custom plugin that dynamically designates layouts for Markdown files.
+ * Right now this means appending `layout: @layouts/Blog.astro` to our blog posts
+ * so we don’t need to do that with each file.
+ */
 function defaultLayoutPlugin() {
-  const layouts = new Set()
+  const layouts = new Set();
   return function (tree, file) {
-    const fileName = file.history[0]
-    const directory = fileName.split("/").slice(-2)[0]
-    const layout =
-      directory.charAt(0).toUpperCase() + directory.slice(1) + ".astro"
+    const fileName = file.history[0];
+    const directory = fileName.split("/").slice(-2)[0];
+    const layout = directory.charAt(0).toUpperCase() + directory.slice(1) + ".astro";
     if (layouts.has(layout) || existsSync(`./src/layouts/${layout}`)) {
-      layouts.add(layout)
-      file.data.astro.frontmatter.layout = `@layouts/${layout}`
+      layouts.add(layout);
+      file.data.astro.frontmatter.layout = `@layouts/${layout}`;
     }
-  }
+  };
 }
 
 // https://astro.build/config
@@ -28,35 +33,29 @@ export default defineConfig({
   site: "https://ddev.com",
   markdown: {
     remarkPlugins: [remarkReadingTime],
-    extendDefaultPlugins: true,
+    extendDefaultPlugins: true
   },
-  integrations: [
-    tailwind(),
-    react(),
-    sitemap(),
-    robotsTxt({
-      sitemap: false,
-      policy: [{ disallow: "/", userAgent: "*" }],
-    }),
-    searchIndex({
-      output: "search.json",
-    }),
-  ],
+  integrations: [tailwind(), react(), sitemap(), robotsTxt({
+    sitemap: false,
+    policy: [{
+      disallow: "/",
+      userAgent: "*"
+    }]
+  }), searchIndex({
+    output: "search.json"
+  }), prefetch()],
   markdown: {
     syntaxHighlight: "shiki",
     // https://github.com/shikijs/shiki/blob/main/docs/languages.md
     shikiConfig: {
-      theme: "nord",
+      theme: "nord"
     },
     remarkPlugins: [defaultLayoutPlugin, remarkReadingTime],
-    rehypePlugins: [
-      widont,
-      plainTextPlugin({
-        contentKey: "plainText",
-        removeEmoji: false,
-        headingTags: ["h2", "h3"],
-      }),
-    ],
-    extendDefaultPlugins: true,
-  },
-})
+    rehypePlugins: [widont, plainTextPlugin({
+      contentKey: "plainText",
+      removeEmoji: false,
+      headingTags: ["h2", "h3"]
+    })],
+    extendDefaultPlugins: true
+  }
+});
