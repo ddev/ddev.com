4c20777d21518f94835d9f40d198efe8dc404d0f
Matt Stein <m@ttste.in>
2023-02-02 09:42:26 -0800

    Update feeds.

--- src/pages/blog/feed.json.js
+++ src/pages/blog/feed.json.js
@@ -1,11 +1,11 @@
 import { getCollection } from "astro:content"
 import { marked } from "marked"
 import { SITE_DESCRIPTION } from "../../config"
-import rss from "@astrojs/rss"
 import sanitizeHtml from "sanitize-html"
 
 const baseUrl = import.meta.env.SITE
 const blogUrl = `${baseUrl}blog`
+
 const posts = await getCollection("blog")
 
 // Get the 50 most recent blog posts
@@ -15,23 +15,26 @@ const recentPosts = posts
   })
   .slice(0, 50)
 
-const items = recentPosts.map((post) => {
+const feedItems = recentPosts.map((post) => {
   return {
-    link: `${blogUrl}/${post.slug}`,
+    id: `${blogUrl}/${post.slug}`,
+    url: `${blogUrl}/${post.slug}`,
     title: post.data.title,
-    content: sanitizeHtml(marked.parse(post.body)),
-    pubDate: post.data.pubDate,
+    content_html: sanitizeHtml(marked.parse(post.body)),
   }
 })
 
-export const get = () =>
-  rss({
-    title: `DDEV Blog`,
-    description: SITE_DESCRIPTION,
-    site: blogUrl,
-    items: items,
-    customData:
-      `<language>en-us</language><lastBuildDate>` +
-      new Date().toUTCString() +
-      `</lastBuildDate>`,
-  })
+const feed = {
+  version: "https://jsonfeed.org/version/1.1",
+  title: `DDEV Blog`,
+  description: SITE_DESCRIPTION,
+  home_page_url: blogUrl,
+  feed_url: `${blogUrl}/feed.json`,
+  items: feedItems,
+}
+
+export async function get({ params, request }) {
+  return {
+    body: JSON.stringify(feed),
+  }
+}
