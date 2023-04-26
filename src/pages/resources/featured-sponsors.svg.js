33b976f59c3f2e72df4cf6e0240283085a4615ec
Matt Stein <m@ttste.in>
2023-04-25 18:07:28 -0700

    Tidy up a bit.

--- src/pages/resources/featured-sponsors.svg.js
+++ src/pages/resources/featured-sponsors.svg.js
@@ -4,30 +4,94 @@ import base64Img from "base64-img"
 
 const baseUrl = import.meta.env.SITE
 
+// Width of the composed SVG
 const overallWidth = 814
+// Maximum height a logo may have
 const maxHeight = 50
+// Maximum width a logo may have
 const maxWidth = 200
+// Horizontal padding between logos
 const xPadding = 40
+// Vertical padding between rows of logos
 const yPadding = 20
 
-let images = []
-let totalHeight = 0
-let currentX = 0
-let currentY = 0
-let rowCount = 1
+/**
+ * Build an SVG response body with rows of evenly-spaced sponsor logos wrapped in anchors.
+ * @returns string
+ */
+const buildResponse = () => {
+  let images = []
+  let totalHeight = 0
+  let currentX = 0
+  let currentY = 0
+  let rowCount = 1
 
-featuredSponsors.map((sponsor, index) => {
-  const dimensions = sizeOf("./public/" + sponsor.logo)
-  const fullW = dimensions.width
-  const fullH = dimensions.height
+  featuredSponsors.map((sponsor, index) => {
+    const dimensions = sizeOf("./public/" + sponsor.logo)
 
-  let h = fullH
-  let w = fullW
-  let ratio = w / h
+    let [w, h] = getScaledImageDimensions(dimensions.width, dimensions.height)
+
+    if (currentX + w + xPadding > overallWidth) {
+      currentX = 0
+      currentY += maxHeight + yPadding
+      rowCount += 1
+    }
+
+    let yOffset = (maxHeight - h) / 2
+
+    images.push({
+      href: baseUrl + sponsor.logo,
+      path: `./public/${sponsor.logo}`,
+      x: currentX,
+      y: currentY + yOffset,
+      height: h,
+      width: w,
+      url: sponsor.url,
+    })
+
+    currentX += w + xPadding
+  })
+
+  totalHeight = (maxHeight + yPadding) * rowCount
+
+  let response = `
+    <svg
+      width="${overallWidth}"
+      height="${totalHeight}"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+    >
+  `
+
+  images.map((image) => {
+    response += `
+      <a xlink:href="${image.url}" target="_blank">
+        <image href="${base64Img.base64Sync(image.path)}" x="${image.x}" y="${image.y}" height="${image.height}" width="${image.width}" />
+      </a>
+    `
+  })
+
+  response += `</svg>`
+
+  return response;
+}
+
+/**
+ * Take the original width and height of an image and proportionally scale it
+ * for optical balance in this layout.
+ * @param {*} width
+ * @param {*} height
+ * @returns [w, h]
+ */
+const getScaledImageDimensions = (width, height) => {
+  let h = height
+  let w = width
+  const ratio = w / h
 
   if (ratio < 1) {
     h = maxHeight
-    w = (maxHeight / fullH) * fullW
+    w = (maxHeight / height) * width
   }
 
   if (ratio === 1) {
@@ -37,68 +101,24 @@ featuredSponsors.map((sponsor, index) => {
 
   if (ratio > 1) {
     h = maxHeight
-    w = (maxHeight / fullH) * fullW
+    w = (maxHeight / height) * width
   }
 
   if (ratio > 2) {
     h = maxHeight * 0.75
-    w = ((maxHeight * 0.75) / fullH) * fullW
+    w = ((maxHeight * 0.75) / height) * width
   }
 
   if (ratio > 3) {
     w = maxWidth
-    h = (maxWidth / fullW) * fullH
+    h = (maxWidth / width) * height
   }
 
-  console.log(sponsor.name + ": " + ratio)
-
-  if (currentX + w + xPadding > overallWidth) {
-    currentX = 0
-    currentY += maxHeight + yPadding
-    rowCount += 1
-  }
-
-  let yOffset = (maxHeight - h) / 2
-
-  images.push({
-    href: baseUrl + sponsor.logo,
-    path: `./public/${sponsor.logo}`,
-    x: currentX,
-    y: currentY + yOffset,
-    height: h,
-    width: w,
-    url: sponsor.url,
-  })
-
-  currentX += w + xPadding
-})
-
-totalHeight = (maxHeight + yPadding) * rowCount
-
-let response = `
-<svg
-  width="${overallWidth}"
-  height="${totalHeight}"
-  version="1.1"
-  xmlns="http://www.w3.org/2000/svg"
-  xmlns:xlink="http://www.w3.org/1999/xlink"
->
-`
-
-images.map((image) => {
-  const imgData = base64Img.base64Sync(image.path)
-
-  response += `
-    <a xlink:href="${image.url}" target="_blank">
-      <image href="${imgData}" x="${image.x}" y="${image.y}" height="${image.height}" width="${image.width}" />
-    </a>
-  `
-})
-
-response += `</svg>`
+  return [w, h];
+}
 
 export async function get({ params, request }) {
   return {
-    body: response,
+    body: buildResponse(),
   }
 }
